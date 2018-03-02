59ac76f21076f31023cde888a9dea9f1e5b7ea1c
MOBILE-OVGFSFC\hayk <hayk.avetisyan@student.howest.be>
2018-03-02 10:10:50 +0100

    Add project files.

---
+++ dotnet.o3sse/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotnet.o3sse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
